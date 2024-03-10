04ff599089b1731c5c447465f6d702a52d9ab584
Abhishek Mondal <100356779+Abhishekmondal256@users.noreply.github.com>
2024-03-10 19:27:14 +0530

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,12 +5,12 @@ try{
 const token=req.cookies.jwtoken;
     console.log(token);
 const verifytoken=jwt.verify(token,process.env.SECRET_KEY);
-    console.log("me);
+    console.log("me");
     console.log(process.env.SECRET_KEY);
-     console.log("mey);
+     console.log("mey");
     console.log(verifytoken);
 const rootUser=await User.findOne({_id:verifytoken._id,"tokens.token":token});
-    console.log("hel); 
+    console.log("hel"); 
      console.log(rootUser);
 if(!rootUser){
     throw new Error("User not found");
