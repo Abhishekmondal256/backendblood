cff3425514dec1f1e62fa06112d5d07e3053e6a5
Abhishek Mondal <100356779+Abhishekmondal256@users.noreply.github.com>
2024-03-11 00:10:57 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -81,8 +81,8 @@ router.post("/signin", async (req, res) => {
       token = await userLogin.generateAuthToken();
       res.cookie("jwtoken", token, {
         expires: new Date(Date.now() + 25892000000),
-        
-
+         httpOnly:true
+           
       })
        
       if (!isMatch) {
