cec24bec33dc2d6e1fcab8ac683a5d21f40d5a83
Abhishek Mondal <100356779+Abhishekmondal256@users.noreply.github.com>
2024-03-13 21:28:35 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -84,6 +84,7 @@ router.post("/signin", async (req, res) => {
       const isMatch = await bcrypt.compare(password, userLogin.password);
 
       token = await userLogin.generateAuthToken();
+      console.log(token);
       res.cookie("jwtoken", token, {
         expires: new Date(Date.now() + 25892000000),
          httpOnly:true
